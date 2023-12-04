13084c5e1af48623b599762b75c12b1d2ba0b56c
Erdem Erdem <yusuf.erd.erdem@gmail.com>
2023-12-04 14:59:31 +0300

    Initiate SGD, GD

--- classifier.c
+++ classifier.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <time.h>		//for random number generator
 #include <ctype.h>		//used only to make text lowercase
 
 #define MAX_LENGTH 511
 #define MAX_QUOTE 100
 #define DICT_SIZE 2500
 
-//TODO: REMOVE SPACES AT THE END OF THE SENTENCE AND DATA CLEARING IS DONE.
+//TODO: 1 means Hayyam, -1 means Shakespeare.
 
 int compute_number_of_words(char *text);
 void text_to_vector(char **dictionary, char **quotes, int **vectors, int wordcount) ;
@@ -20,6 +22,16 @@ int check_occurence(char **dictionary, char *word, int check_until);
 void make_lowercase(char **quotes);
 void organize_add(char **quotes);
 
+double dot_product(int *vector, double *parameters, int wordcount);
+double f_x(int *vector, double *parameters, int wordcount);
+void initiate_param(double *parameters, int wordcount);
+
+void gradiend_descent(int **vectors, double *parameters, int wordcount, int *labels, double *gradients, double stepsize, int maxiter, double error);
+void stoc_grad_desc();
+void adam();
+
+void compute_gradient(int **vectors, double *parameters, int wordcount, int *labels, double *gradients);
+double compute_loss(int *vector, double *parameters, int wordcount, int *labels);
 
 int main(){
 	int i=0, j;
@@ -32,10 +44,12 @@ int main(){
 	}
 
 	char **quotes, **wordsdic;
-	int **wordvectors;
+	int **wordvectors, *labels;			//qoutes[i]'s label is labels[i]. (either 1 or -1)
+	double *parameters, *gradients;
+	
 
 	quotes = (char**) malloc(MAX_QUOTE*sizeof(char*));				//Allocating memory for quotes, dictionary and vectors.
-	if(quotes==NULL){
+	if(quotes == NULL){
 		printf("Memory allocation failed. Exiting.");
 		return 1;
 	}
@@ -44,7 +58,7 @@ int main(){
 	}
 	
 	wordsdic = (char**)malloc(DICT_SIZE*sizeof(char*));
-	if(wordsdic==NULL){
+	if(wordsdic == NULL){
 		printf("Memory allocation failed. Exiting.");
 		return 1;
 	}
@@ -53,7 +67,7 @@ int main(){
 	}
 	
 	wordvectors = (int**) malloc(MAX_QUOTE*sizeof(int*));
-	if(wordvectors==NULL){
+	if(wordvectors == NULL){
 		printf("Memory allocation failed. Exiting.");
 		return 1;
 	}
@@ -65,6 +79,14 @@ int main(){
 		fgets(quotes[i], MAX_LENGTH, fptr);
 	}
 	fclose(fptr);
+	
+	labels = (int*) malloc(MAX_QUOTE*sizeof(int));
+	if(labels == NULL){
+		printf("Memory allocation failed. Exiting.");
+		return 1;
+	}
+	
+
 	for( i=0; i<MAX_QUOTE; i++ ){
 		remove_punctuation(quotes[i]);
 		remove_multiple_spaces(quotes[i], strlen(quotes[i]));
@@ -96,32 +118,213 @@ int main(){
         printf("\n%d: %s", i + 1, wordsdic[i]);
     }
     
-	text_to_vector(wordsdic, quotes, wordvectors, wordcount);		//BURADA BÝR SORUN VAR.	I = 0 VE J = 720'DE BÝTÝRÝYOR.
+	text_to_vector(wordsdic, quotes, wordvectors, wordcount);
 	
-	for( i=0; i<MAX_QUOTE; i++ ){
+	/*for( i=0; i<MAX_QUOTE; i++ ){
     	printf("\n%d. METIN: %s\n%d. METNIN VEKTOR HALI:\n", i+1, quotes[i] ,i+1);
     	for (j = 0; j < wordcount; j++) {
         	printf("%s: %d\n", wordsdic[j], wordvectors[i][j]);
     	}
+	}*/
+	
+	
+	
+	parameters = (double*) malloc(wordcount*sizeof(double));
+	if( parameters == NULL ){
+		printf("Memory allocation failed. Exiting."); 
+		return 1;
+	}
+	initiate_param(parameters, wordcount);
+	gradients = (double*) malloc(wordcount*sizeof(double));
+	if( gradients == NULL ){
+		printf("Memory allocation failed. Exiting."); 
+		return 1;
+	}
+	for( i=0; i<wordcount; i++ ){
+		gradients[i] = 0;
+	}
+	for( i=0; i<MAX_QUOTE; i++ ){
+		labels[i] = 1;
 	}
 	
 	
-	printf("A");
+	
+	stoc_grad_desc(wordvectors, parameters, wordcount, labels, gradients, 0.01, 10000, 0.01);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	for( i=0; i<MAX_QUOTE; i++ ){
 		free(quotes[i]);
 	}
 	free(quotes);
-	printf("B");
 	for( i=0; i<DICT_SIZE; i++ ){				//FREEING MEMORY.
 		free(wordsdic[i]);
 	}
 	free(wordsdic);
 	for( i=0; i<MAX_QUOTE; i++ ){
-		free(wordsdic[i]);
+		free(wordvectors[i]);
 	}
-	free(wordsdic);
+	free(wordvectors);
 	
 	return 0;
+}
+
+void gradiend_descent(int **vectors, double *parameters, int wordcount, int *labels, double *gradients, double stepsize, int maxiter, double error){
+	int i=0, t, z, stop = 1, detect = 1;
+	double y_hat_std, y_std, gradient, total_loss;					//YAPAMAZSAN NONLINEAR REGRESSION DENE.			(JACOBIAN ILE)
+	while( i<maxiter && detect ){
+		
+		for( t=0; t<wordcount; t++ ){
+			gradient = 0;
+			
+			for( z=0; z<MAX_QUOTE; z++ ){
+				
+				y_std = (labels[z]+1) / 2;
+				y_hat_std = ( f_x(vectors[z], parameters, wordcount)+1 ) / 2;
+				
+				gradient = ( y_hat_std - y_std ) * vectors[z][t] ;
+				
+			}
+			gradient /= MAX_QUOTE;
+			parameters[t] -= stepsize*gradient;					//asd stands for gradients.
+			
+			
+			
+		}
+		
+		total_loss = 0;
+		for( t=0; t<MAX_QUOTE; t++ ){
+			total_loss += compute_loss(vectors[t], parameters, wordcount, labels);
+		}
+		total_loss /= MAX_QUOTE;
+		printf("\nIteration %d: 	Loss: %lf", i+1, total_loss);
+		
+		/*y_hat = (labels[i]+1) / 2;
+		y_predicted = ;
+		
+		for( j=0; j<wordcount; j++ ){
+			parameters[j] -= stepsize*gradients[j];				//Hareket et.
+		}
+		
+		j=0;  detect = 1;
+		while( j++<wordcount && detect ){					//Eðer bütün boyutlarda hareket miktarý hatadan küçükse çýk.
+			if( stepsize*gradients[j] > error )		detect = 0;
+		}
+		if( detect ) 	stop = 0;
+		*/
+		i++;
+	}
+	printf("STOP = %d", stop);
+	
+}
+
+void stoc_grad_desc(int **vectors, double *parameters, int wordcount, int *labels, double *gradients, double stepsize, int maxiter, double error){
+	int i=0, t, z, stop = 1, detect = 1, chosen, j;
+	double y_hat_std, y_std, gradient, total_loss;
+	srand(time(NULL));
+	while( i<maxiter && stop ){
+		chosen = rand()%MAX_QUOTE;
+		y_std = (labels[chosen]+1) / 2;
+		y_hat_std = ( f_x(vectors[chosen], parameters, wordcount)+1 ) / 2;
+		gradient = 0;
+		detect = 1;				//detect 1 yapýlýr. Deðiþim miktarý hatadan büyük olan tek bir örnek varsa detect 0 olur. Böylece tüm boyutlarda hatanýn altýna inilene dek programda kalýnýr.
+		for( j=0; j<wordcount; j++ ){
+			
+			gradient = ( y_hat_std - y_std ) * vectors[chosen][j];
+			parameters[chosen] -= stepsize*gradient;
+			
+			//printf("\n chosen = %d	j = %d  stepsize = %lf	gradient = %lf	st*gr = %lf		error = %lf	detect = %d", chosen, j, stepsize, gradient, stepsize*gradient, error, detect);
+			if( stepsize*gradient > error ){
+				detect = 0;
+			}
+			
+		}
+		total_loss = 0;
+		for( t=0; t<MAX_QUOTE; t++ ){
+			total_loss += compute_loss(vectors[t], parameters, wordcount, labels);
+		}
+		total_loss /= MAX_QUOTE;
+		printf("\nIteration %d: 	Loss: %lf", i+1, total_loss);
+		
+		if( detect ){
+			stop = 1;
+		}
+		
+		
+		
+		
+		
+		
+		
+		i++;
+	}
+	
+}
+
+
+
+
+
+void compute_gradient(int **vectors, double *parameters, int wordcount, int *labels, double *gradients){				//COMPUTES GRADIENT FOR ONE DIMENSION
+	int i, j;
+	double f, err;
+	for( i=0; i<MAX_QUOTE; i++ ){
+		f = f_x( vectors[i], parameters, wordcount );
+		err = f - labels[i];
+		for( j=0; j<wordcount; j++ ){
+			gradients[j] += err * vectors[i][j];
+		}
+		gradients[i] /= MAX_QUOTE;
+	}
+}
+
+double compute_loss(int *vector, double *parameters, int wordcount, int *labels){		//TEK BIR ORNEK IÇIN HATA HESAPLAR.
+	int i;
+	double loss = 0, fx = f_x(vector, parameters, wordcount);
+	double fx_std = (fx+1.0) / 2.0;
+	
+	if( fx_std == 0 ) fx_std += 0.00001;
+	if( fx_std == 1 ) fx_std -= 0.00001;
+	for( i=0; i<MAX_QUOTE; i++ ){
+		//printf("\n1: %d		2: %lf		3: %lf", labels[i], log (sigm), log( 1-sigm ));
+		loss += labels[i]*log (fx_std) + ( 1-labels[i] )*log( 1-fx_std );
+		//printf("\ni = %d	loss = %lf", i, loss);
+	}
+	return -loss;
+}
+
+double dot_product(int *vector, double *parameters, int wordcount){
+	int i;
+	double result = 0;
+	for( i=0; i<wordcount; i++ ){
+		result += vector[i]*parameters[i];
+	}
+	return result;
+}
+
+double f_x(int *vector, double *parameters, int wordcount){			//Sýgmoid function.
+	double dot = dot_product(vector, parameters, wordcount);
+	return tanh(dot);
+}
+
+void initiate_param(double *parameters, int wordcount){				//TODO: BU SADECE INT YAPIYOR. DOUBLE YAPMANIN YOLUNU BUL
+	int i;
+	double random_value;
+	srand(time(NULL));
+	for( i=0; i<wordcount; i++ ){
+		random_value = (double)rand()/RAND_MAX*2.0-1.0;
+		random_value *= 5;				//float in range -1 to 1
+		parameters[i] = 0.0;
+	}
 }
 
 int fill_dictionary(char **dictionary, char **quotes) {
